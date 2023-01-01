db11d3d68db7bc55f66b90e0c167d22b7d112746
dudu <whqtkf12@gmail.com>
2023-01-01 18:09:57 +0900

    Initial Commit

---
+++ Twitter SwiftUI Clone Udemy/Twitter_SwiftUI_Clone_UdemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_SwiftUI_Clone_UdemyApp.swift
+//  Twitter SwiftUI Clone Udemy
+//
+//  Created by dudu on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_SwiftUI_Clone_UdemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
