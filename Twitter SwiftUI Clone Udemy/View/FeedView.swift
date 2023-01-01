f4d499c28dcf054b810d6451e43f1238c2d9dff1
dudu <whqtkf12@gmail.com>
2023-01-01 22:45:09 +0900

    feat: FeedView 생성 및, Button 구현

---
+++ Twitter SwiftUI Clone Udemy/View/FeedView.swift
@@ -0,0 +1,36 @@
+//
+//  FeedView.swift
+//  Twitter SwiftUI Clone Udemy
+//
+//  Created by dudu on 2023/01/01.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                
+            }
+            
+            Button(action: {}) {
+                Image("tweet")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 32, height: 32)
+                    .padding()
+            }
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .padding()
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
