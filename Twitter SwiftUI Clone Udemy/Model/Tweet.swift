271de85213061f8ab76d08e2198cfca7ccf8b94a
dudu <whqtkf12@naver.com>
2023-01-23 23:57:10 +0900

    feat: Tweet Model 구현

---
+++ Twitter SwiftUI Clone Udemy/Model/Tweet.swift
@@ -0,0 +1,31 @@
+//
+//  Tweet.swift
+//  Twitter SwiftUI Clone Udemy
+//
+//  Created by dudu on 2023/01/23.
+//
+
+import Foundation
+import Firebase
+
+struct Tweet: Identifiable {
+    let id: String
+    let username: String
+    let profileImageUrl: String
+    let fullname: String
+    let caption: String
+    let lieks: Int
+    let uid: String
+    let timestamp: Timestamp
+    
+    init(dictionary: [String: Any]) {
+        self.id = dictionary["uid"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+        self.fullname = dictionary["fullname"] as? String ?? ""
+        self.caption = dictionary["caption"] as? String ?? ""
+        self.lieks = dictionary["likes"] as? Int ?? 0
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: .now)
+    }
+}
