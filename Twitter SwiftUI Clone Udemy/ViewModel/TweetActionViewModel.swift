684574b634241b25592d17fd453929604f37c2c5
dudu <whqtkf12@naver.com>
2023-01-24 17:00:29 +0900

    feat: checkIfUserLikeTweet 매서드구현

--- Twitter SwiftUI Clone Udemy/ViewModel/TweetActionViewModel.swift
+++ Twitter SwiftUI Clone Udemy/ViewModel/TweetActionViewModel.swift
@@ -16,23 +16,47 @@ final class TweetActionViewModel: ObservableObject {
     
     init(tweet: Tweet) {
         self.tweet = tweet
+        Task {
+            await checkIfUserLikedTweet()
+        }
     }
     
     
     func likeTweet() async {
-        guard let uid = AuthViewModel.shared.userSession?.uid else {
-            return
-        }
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
         let tweetLikesRef = tweetCollection.document(tweet.id).collection("tweet-likes")
         let userLikesRef = userCollection.document(uid).collection("user-likes")
         
         try! await tweetCollection.document(tweet.id).updateData(["likes:" : tweet.likes + 1])
         try! await tweetLikesRef.document(uid).setData([:])
         try! await userLikesRef.document(tweet.id).setData([:])
-        self.didLike = true
+        
+        await MainActor.run {
+            self.didLike = true
+        }
     }
     
-    func unlikeTweet() {
+    func unlikeTweet() async {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let tweetLikesRef = tweetCollection.document(tweet.id).collection("tweet-likes")
+        let userLikesRef = userCollection.document(uid).collection("user-likes")
         
+        try! await tweetCollection.document(tweet.id).updateData(["likes:" : tweet.likes - 1])
+        try! await tweetLikesRef.document(uid).delete()
+        try! await userLikesRef.document(tweet.id).delete()
+        
+        await MainActor.run {
+            self.didLike = false
+        }
+    }
+    
+    private func checkIfUserLikedTweet() async {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let userLikesRef = userCollection.document(uid).collection("user-likes").document(tweet.id)
+        
+        let snapshot = try! await userLikesRef.getDocument()
+        await MainActor.run {
+            self.didLike = snapshot.exists
+        }
     }
 }
